325e9ee0189c21f8fb80f9dbd00a5e31fea46cf9
unknown <ramiropen.work@gmail.com>
2021-11-23 21:08:56 -0800

    finished

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,75 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+let yourName = "Ramiro Peña"
 
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let sugar = 0 // Sugar Sprinkle
 
-// Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+let addGbBtn = document.getElementById("add-gb")
+let decreaseGbBtn = document.getElementById("minus-gb")
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
+let addCcBtn = document.getElementById("add-cc")
+let decreaseCcBtn = document.getElementById("minus-cc")
+
+let addSugarBtn = document.getElementById("add-sugar")
+let decreaseSugarBtn = document.getElementById("minus-sugar")
+
+let totalVotesTd = document.querySelector('#qty-total');
+totalVotesTd.textContent = gb + cc + sugar;
+
+ 
+document.getElementById('credit').textContent = `Created by ${'Ramiro Peña'}`
+
+
+
+
+// Gingerbread add + subtract buttons
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+    gb = gb +1;
+    console.log('gb');
+    document.querySelector('#qty-gb').textContent = gb;
+    totalVotesTd.textContent = gb + cc + sugar;
+})
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+document.getElementById('minus-gb').addEventListener('click', function() {
+    gb = gb -1;
+    console.log('gb');
+    document.querySelector('#qty-gb').textContent = gb;
+    totalVotesTd.textContent = gb + cc + sugar;
+})
+
+
+
+// chocolate chip add + subtract buttons
+document.getElementById('add-cc').addEventListener('click', function() {
+    cc = cc +1;
+    console.log('cc');
+    document.querySelector('#qty-cc').textContent = cc;
+    totalVotesTd.textContent = gb + cc + sugar;
+})
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    cc = cc -1;
+    console.log('cc');
+    document.querySelector('#qty-cc').textContent = cc;
+    totalVotesTd.textContent = gb + cc + sugar;
+})
+
+
+// Sugar Sprinkle add + subtract buttons
+document.getElementById('add-sugar').addEventListener('click', function() {
+    sugar = sugar +1;
+    console.log('sugar');
+    document.querySelector('#qty-sugar').textContent = sugar;
+    totalVotesTd.textContent = gb + cc + sugar;
+})
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    sugar = sugar -1;
+    console.log('sugar');
+    document.querySelector('#qty-sugar').textContent = sugar;
+    totalVotesTd.textContent = gb + cc + sugar;
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
